81e1f98bc6d8555b9200670ce3d882101d9fafd0
kristihu <kristihu@metropolia.fi>
2024-06-03 18:42:59 +0300

    initial commit

---
+++ Osa1/anekdootit/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.',
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+  const [winner, setWinner] = useState(null);
+
+  const randomChoice = () => {
+    const random = Math.floor(Math.random() * anecdotes.length);
+    console.log(random);
+    setSelected(random);
+  };
+
+  const voteForAnecdote = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
+  useEffect(() => {
+    const calculateWinner = () => {
+      const maxVotes = Math.max(...votes);
+      const winIndex = votes.indexOf(maxVotes);
+      setWinner(winIndex);
+    };
+
+    calculateWinner();
+  }, [votes]);
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <br></br>
+      <p>Votes: {votes[selected]}</p>
+      <button onClick={randomChoice}>next anecdote</button>
+      <button onClick={voteForAnecdote}>Vote</button>
+      <h1>Anecdote with most votes</h1>
+      {votes[winner] > 0 && (
+        <>
+          <p>{anecdotes[winner]}</p>
+          <p>has {votes[winner]} votes</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
